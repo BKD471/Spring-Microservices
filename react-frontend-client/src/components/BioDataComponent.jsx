e4debeb3f2655fd2e7d32dbf288826cf7db22173
BKD471 <bhaskarkumardas9@gmail.com>
2023-03-05 11:46:06 +0530

    removing unncessaary imports

--- react-frontend-client/src/components/BioDataComponent.jsx
+++ react-frontend-client/src/components/BioDataComponent.jsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React, { useEffect,useState } from 'react';
 import {EMPLOYEE_SERVICE_BASE_URL,EMPLOYEE_ID} from "../configs/Constants"
 import DepartmentComponent from './DepartmentComponent';
